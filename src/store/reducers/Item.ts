761a1f4710f7480b47c89420bd9ad0283dd979e7
granddevv <spacingflight@outlook.com>
2018-04-29 23:17:41 +0200

    Add Reducer

---
+++ src/store/reducers/Item.ts
@@ -0,0 +1,31 @@
+import Item from '../models/Item';
+import { ActionTypes, Action} from '../action/item';
+export interface State {
+    items: Item[]
+}
+
+export const initialState: State = {
+    items: []
+}
+
+export function itemReducer( state: State = initialState, action: Action) {
+    switch (action.type) {
+        case ActionTypes.ADD_ITEM: {
+            const item = action.payload.item;
+            return {
+                ...state,
+                items: [...state.items, item]
+            }
+        }
+        case ActionTypes.DELETE_ITEM: {
+            const itemIndex = action.payload.itemIndex;
+            return {
+                ...state,
+                items: state.items.splice(itemIndex, 1)
+            }
+        }
+        default:
+            return state;
+
+    }
+}
