42bfddf4555c334c5885c232f6cf0bd675d3f26c
granddevv <spacingflight@outlook.com>
2018-04-30 04:56:34 +0200

    Add Header and some changes

--- src/components/Header/Header.tsx
+++ src/components/Header/Header.tsx
@@ -1,5 +1,6 @@
 import * as React from 'react';
-
+import Logo from '../Common/Logo';
+import './Header.css';
 interface HeaderProps {
     headerTitle: string
 }
@@ -8,8 +9,9 @@ class Header extends React.Component<HeaderProps> {
 
     public render() {
         return (
-            <div className="App">
-                <p className="App-intro">
+            <div className="header">
+                <Logo logoUrl="http://brandmark.io/logo-rank/random/pepsi.png" />
+                <p className="header-title">
                     {this.props.headerTitle}
                 </p>
             </div>
