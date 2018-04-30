d5c3a18c6ae2ac5519f3dc4d6ad6ad1576660e8e
granddevv <spacingflight@outlook.com>
2018-04-30 04:57:30 +0200

    Change App Component

--- src/App.tsx
+++ src/App.tsx
@@ -1,16 +1,12 @@
 import * as React from 'react';
 import './App.css';
-
-import logo from './logo.svg';
+import Header from './components/Header/Header';
 
 class App extends React.Component {
   public render() {
     return (
       <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Welcome to React</h1>
-        </header>
+        <Header headerTitle="Hello This is TypeScript React App" />
         <p className="App-intro">
           To get started, edit <code>src/App.tsx</code> and save to reload.
         </p>
