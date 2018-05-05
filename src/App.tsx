e4ff83914e43bc05e90798dddac1fdf90f6bcd65
granddevv <spacingflight@outlook.com>
2018-05-05 16:40:39 +0200

    Add App Component

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,18 @@
 import * as React from 'react';
 import './App.css';
 import Header from './components/Header/Header';
+import DataForm from './components/Common/DataForm';
+import ItemList from './components/Common/ItemList';
 
 class App extends React.Component {
   public render() {
     return (
       <div className="App">
         <Header headerTitle="Hello This is TypeScript React App" />
-        <p className="App-intro">
+        <DataForm firstName="big" lastName="Star" birthday="1999292" phoneNumber="+227837483" />
+        <p>Here is list of Items</p>
+        <ItemList />
+          <p className="App-intro">
           To get started, edit <code>src/App.tsx</code> and save to reload.
         </p>
       </div>
