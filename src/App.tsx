da439620d982a1b6b876c00b6641d19aa3ff8bf7
granddevv <spacingflight@outlook.com>
2018-05-07 19:13:41 +0200

    Change App Component

--- src/App.tsx
+++ src/App.tsx
@@ -1,23 +1,38 @@
 import * as React from 'react';
 import './App.css';
-import Header from './components/Header/Header';
-import DataForm from './components/Common/DataForm';
-import ItemList from './components/Common/ItemList';
+import HeaderComponent from './components/Header/HeaderComponent';
+// import DataForm from './components/Common/DataForm';
+// import ItemList from './components/Common/ItemList';
+
+import { Layout } from 'antd';
+
+import LeftMenu from './components/LeftMenu/LeftMenu';
+import ContentComponent from './components/Content/ContentComponent';
 
 class App extends React.Component {
   public render() {
     return (
       <div className="App">
-        <Header headerTitle="Hello This is TypeScript React App" />
-        <DataForm firstName="big" lastName="Star" birthday="1999292" phoneNumber="+227837483" />
-        <p>Here is list of Items</p>
-        <ItemList />
-          <p className="App-intro">
-          To get started, edit <code>src/App.tsx</code> and save to reload.
-        </p>
+          <Layout style={{display: 'initial'}}>
+              <HeaderComponent />
+              <Layout>
+                  <LeftMenu />
+                  <ContentComponent />
+              </Layout>
+          </Layout>
       </div>
     );
   }
 }
 
 export default App;
+
+
+{/*<DataForm firstName="big" lastName="Star" birthday="1999292" phoneNumber="+227837483" />*/}
+{/*<p>Here is list of Items</p>*/}
+{/*<div>*/}
+    {/*<ItemList />*/}
+    {/*<p className="App-intro">*/}
+        {/*To get started, edit <code>src/App.tsx</code> and save to reload.*/}
+    {/*</p>*/}
+{/*</div>*/}
